3f047c146a9ec65586e5471540c3828d523e01ef
Furkan Berkehan <furkanberkehan38@gmail.com>
2023-03-07 17:26:51 +0300

    TO AFAD API
    Sistem AFAD olacak şekilde güncellendi.

--- main.js
+++ main.js
@@ -1,95 +1,101 @@
 
+const tableBody = document.getElementById("verileriDondur");
+let depremData = [];
 
-    halfmoon.toggleDarkMode()
+function showDepremData() {
+  tableBody.innerHTML = "";
+  if (depremData.length === 0) {
+    document.getElementById("veriUyarisi").style.display = "block";
+    return;
+  }
+  document.getElementById("veriUyarisi").style.display = "none";
+  // console.log(depremData);
+  depremData.forEach((deprem) => {
 
-
-
-    fetch("https://api.orhanaydogdu.com.tr/deprem/live.php")
-  .then(response => response.json())
-  .then(data => {
-
-
-    for (let depremVerileri = 0; depremVerileri < data.result.length; depremVerileri++) {
-
-
-        // console.log(data.result[depremVerileri]);
-
-        const givenDate = new Date(data.result[depremVerileri].date);
-        const currentDate = new Date();
-        const diffInMinutes = (currentDate - givenDate) / 1000 / 60;
-        const hours = Math.floor(diffInMinutes / 60);
-        const minutes = Math.floor(diffInMinutes % 60);
-
-     
-
-        if (hours<1) {
-          if (minutes<6) {
-
-            if (data.result[depremVerileri].mag>0 && data.result[depremVerileri].mag<4) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td style="color:red;" class="blink">'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-primary"><b><b>'+data.result[depremVerileri].mag+'</b></b></span></td></tr>');  
-            }
-            if (data.result[depremVerileri].mag>4 && data.result[depremVerileri].mag<5) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td style="color:red;" class="blink">'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-secondary"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-            }
-            if (data.result[depremVerileri].mag>5 && data.result[depremVerileri].mag<10) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td style="color:red;" class="blink">'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-danger"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-
-              }
-            
-          }else{
-            if (data.result[depremVerileri].mag>0 && data.result[depremVerileri].mag<3) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-primary"><b><b>'+data.result[depremVerileri].mag+'</b></b></span></td></tr>');  
-            }
-            if (data.result[depremVerileri].mag>3 && data.result[depremVerileri].mag<4) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-secondary"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-               }
-            if (data.result[depremVerileri].mag>4 && data.result[depremVerileri].mag<10) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-danger"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-              }
-          }
-            
-        }
-        else{
-
-          
-            if (data.result[depremVerileri].mag>0 && data.result[depremVerileri].mag<3) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+hours + " sa. " + minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-primary"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-            }
-            if (data.result[depremVerileri].mag>3 && data.result[depremVerileri].mag<4) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+hours + " sa. " + minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-secondary"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
+    function formatTarih(tarih) {
+  const d = new Date(tarih);
+  const gun = d.getDate().toString().padStart(2, "0");
+  const ay = (d.getMonth() + 1).toString().padStart(2, "0");
+  const yil = d.getFullYear();
+  return `${gun}/${ay}/${yil}`;
 }
-            if (data.result[depremVerileri].mag>4 && data.result[depremVerileri].mag<10) {
-                $('#verileriDondur').append('<tr  onClick="showMap('+data.result[depremVerileri].lat+','+data.result[depremVerileri].lng+','+'\''+data.result[depremVerileri].lokasyon.replace("(", "").replace(")", "")+'\''+','+data.result[depremVerileri].mag+','+'\''+data.result[depremVerileri].date+'\''+')"><th>'+data.result[depremVerileri].lokasyon+'</th><td>'+hours + " sa. " + minutes + " dk."+' önce</td><td><span style="background-color: black;border: 1px solid white;text-align:center;" class="badge badge-primary"><b><b>'+data.result[depremVerileri].depth+' KM'+'</b></b></span></td><td><span class="badge badge-danger"><b>'+data.result[depremVerileri].mag+'</b></span></td></tr>');  
-            
-                }
+
+function formatSaat(tarih) {
+  const d = new Date(tarih);
+  const saat = d.getHours().toString().padStart(2, "0");
+  const dakika = d.getMinutes().toString().padStart(2, "0");
+  return `${saat}:${dakika}`;
+}
 
 
-            
-        }
 
-        
+
+    const row = `
+      <tr onClick="showMap('${deprem.latt}','${deprem.lonn}','${deprem.lokasyon.replace("(", "").replace(")", "")}','${deprem.siddet}','${deprem.tarih}')">
+        <td>${deprem.lokasyon}</td>
+        <td>${formatTarih(deprem.tarih)}<br/>${formatSaat (deprem.tarih)}</td>
+        <td><span class="badge">${deprem.depth} km</span></td>
+        <td><span class="badge">${deprem.siddet}</span></td>
+      </tr>
+    `;
+
+
+    tableBody.innerHTML += row;
+  });
+}
+
+function fetchDeprems() {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const start = yesterday.toISOString().slice(0, 19).replace("T", " ");
+  const end = today.toISOString().slice(0, 19).replace("T", " ");
+
+  const url = `https://cors-anywhere.herokuapp.com/https://deprem.afad.gov.tr/apiv2/event/filter?start=${start}&end=${end}&limit=10000&offset=5&orderby=timedesc`;
+
+
+
+  fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("API'den veriler alınamadı");
     }
-
-    
-   
-// Tablonun ID'sini seçin
-const tabloKontrol = document.getElementById("myTable");
-
-// Tablonun satır sayısını alın
-const rowCount = tabloKontrol.rows.length;
-// Eğer satır sayısı 0 ise uyarı mesajı gösterin
-if (rowCount < 1) {
-  alert("Tabloda veri yok!");
-  document.getElementById('veriUyarisi').style.display="block";
-}
-
-
-    
-
+    return response.json();
   })
-  .catch(error => {
+  .then((data) => {
+   
+    depremData = data.map((deprem) => {
+      return {
+        lokasyon: deprem.location,
+        tarih: deprem.date.replace('T', ' '),
+        depth: deprem.depth,
+        siddet: deprem.magnitude,
+        latt: deprem.latitude,
+        lonn: deprem.longitude,
+      };
+    });
+    showDepremData();
+    const loading = document.getElementById("loading");
+    loading.style.display = "none";
+  })
+  .catch((error) => {
     console.error(error);
   });
+
+}
+
+fetchDeprems();
+
+
+
+
+
+
+
+
+
+
 
 
 // Get the table and the buttons
@@ -144,40 +150,37 @@ function filterTable(table, max, min, between) {
 function showMap(lat, lng, konum, buyukluk, tarih) {
 
 
- const konumPopup = document.getElementById("konumPopup");
- const tarihPopup = document.getElementById("tarihPopup");
- const siddetPopup = document.getElementById("siddetPopup");
- const tehlikeDurumuPopup = document.getElementById("tehlikeDurumuPopup");
- const Yapilacaklarr = document.getElementById("Yapilacaklarr");
+const konumPopup = document.getElementById("konumPopup");
+const tarihPopup = document.getElementById("tarihPopup");
+const siddetPopup = document.getElementById("siddetPopup");
+const tehlikeDurumuPopup = document.getElementById("tehlikeDurumuPopup");
+const Yapilacaklarr = document.getElementById("Yapilacaklarr");
 
 
 
- const morkutucuk = document.getElementById("morkutucuk");
+const morkutucuk = document.getElementById("morkutucuk");
 
- morkutucuk.classList.add("animate__animated");
+morkutucuk.classList.add("animate__animated");
 
- konumPopup.innerHTML = konum;
- tarihPopup.innerHTML = tarih;
- siddetPopup.innerHTML = buyukluk;
-
-
- if (buyukluk>6) {
-  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-danger" >Tehlikeli</span> Olarak nitelendirildi.';
-  Yapilacaklarr.innerHTML = '<li>Bölgedeki çoğu ev yıkılmış olabilir!</li><li>Yardıma ihtiyacı olan insanlar olabilir!</li><li>Çevre illerden de hissedilen güçlü bir deprem! Yerel afet birimlerine bilgi verin!</li>';
- }
-
- if (buyukluk>4 && buyukluk<6) {
-  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-secondary" >Şiddetli</span> Olarak nitelendirildi.';
-  Yapilacaklarr.innerHTML = '<li>Çoğu kişinin hissettiği güçlü bir sarsılma yaşandı.</li><li>Problemli ve eski evler yıkılmış olabilir.</li><li>Bazı eşyalar düşmüş olabilir.</li><li>Yardıma ihtiyacı olan insanlar bulunabilir.</li>';
- }
-
- if (buyukluk<4) {
-  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-primary" >Hafif Sarsıntı</span> Olarak nitelendirildi.';
-  Yapilacaklarr.innerHTML = '<li>Bazı kişiler hafif bir sarsılma hissetmiş olabilir.</li><li>Ciddi bir sarsıntı olmadı</li>';
- }
+konumPopup.innerHTML = konum;
+tarihPopup.innerHTML = tarih;
+siddetPopup.innerHTML = buyukluk;
 
 
+if (buyukluk>6) {
+ tehlikeDurumuPopup.innerHTML = '<span class="badge badge-danger" >Tehlikeli</span> Olarak nitelendirildi.';
+ Yapilacaklarr.innerHTML = '<li>Bölgedeki çoğu ev yıkılmış olabilir!</li><li>Yardıma ihtiyacı olan insanlar olabilir!</li><li>Çevre illerden de hissedilen güçlü bir deprem! Yerel afet birimlerine bilgi verin!</li>';
+}
 
+if (buyukluk>4 && buyukluk<6) {
+ tehlikeDurumuPopup.innerHTML = '<span class="badge badge-secondary" >Şiddetli</span> Olarak nitelendirildi.';
+ Yapilacaklarr.innerHTML = '<li>Çoğu kişinin hissettiği güçlü bir sarsılma yaşandı.</li><li>Problemli ve eski evler yıkılmış olabilir.</li><li>Bazı eşyalar düşmüş olabilir.</li><li>Yardıma ihtiyacı olan insanlar bulunabilir.</li>';
+}
+
+if (buyukluk<4) {
+ tehlikeDurumuPopup.innerHTML = '<span class="badge badge-primary" >Hafif Sarsıntı</span> Olarak nitelendirildi.';
+ Yapilacaklarr.innerHTML = '<li>Bazı kişiler hafif bir sarsılma hissetmiş olabilir.</li><li>Ciddi bir sarsıntı olmadı</li>';
+}
 
 
 
@@ -186,11 +189,9 @@ document.querySelector("iframe").src = src;
 const mapGoster = document.getElementById("mapGoster");
 mapGoster.style.display="flex";
 
-
-
-
-
 }
+
+
 
 
 function showMapClose() {
@@ -207,10 +208,50 @@ mapGoster.style.display="none";
 
 
 
-window.onload = function () {
 
-  const loading = document.getElementById("loading");
-  loading.style.display="none";
+var darkModeMu = 0;
+
+function darkModeGecis() {
+
+
+  if (darkModeMu == 0) {
+  halfmoon.toggleDarkMode();
+
+  document.querySelector('.content-wrapper').style.removeProperty('background-color');
+  document.querySelector('#morkutucuk').style.removeProperty('background-color');
+  document.querySelector('#morkutucuk').style.removeProperty('border');
+  document.querySelector('#kapatbutton').style.removeProperty('backgroundColor');
+  document.querySelector('#kapatbutton').style.removeProperty('color');
+
+  document.querySelector('.content-wrapper').style.setProperty('background-color', 'white');
   
-    
-  };
+  document.querySelector('#morkutucuk').style.setProperty('background-color', 'white');
+  
+  document.querySelector('#morkutucuk').style.setProperty('border', '1px solid black');
+  
+  document.querySelector('#kapatbutton').style.setProperty('background-color', 'white');
+  
+  darkModeMu++;
+}else {
+  halfmoon.toggleDarkMode();
+
+  document.querySelector('.content-wrapper').style.removeProperty('background-color');
+  document.querySelector('#morkutucuk').style.removeProperty('background-color');
+  document.querySelector('#morkutucuk').style.removeProperty('border');
+  document.querySelector('#kapatbutton').style.removeProperty('backgroundColor');
+  document.querySelector('#kapatbutton').style.removeProperty('color');
+
+
+document.querySelector('.content-wrapper').style.setProperty('background-color', 'black');
+
+document.querySelector('#morkutucuk').style.setProperty('background-color', 'black');
+
+document.querySelector('#morkutucuk').style.setProperty('border', '1px solid #6f009d');
+
+document.querySelector('#kapatbutton').style.setProperty('background-color', '#6f009d');
+
+darkModeMu = 0;
+}
+
+
+}
