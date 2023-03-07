0e3e3b8096f5be41dc1e6e0136ef2aad5a40c975
Furkan Berkehan <furkanberkehan38@gmail.com>
2023-03-07 17:37:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-
 const tableBody = document.getElementById("verileriDondur");
 let depremData = [];
-
 function showDepremData() {
   tableBody.innerHTML = "";
   if (depremData.length === 0) {
@@ -26,10 +24,6 @@ function formatSaat(tarih) {
   const dakika = d.getMinutes().toString().padStart(2, "0");
   return `${saat}:${dakika}`;
 }
-
-
-
-
     const row = `
       <tr onClick="showMap('${deprem.latt}','${deprem.lonn}','${deprem.lokasyon.replace("(", "").replace(")", "")}','${deprem.siddet}','${deprem.tarih}')">
         <td>${deprem.lokasyon}</td>
@@ -38,24 +32,16 @@ function formatSaat(tarih) {
         <td><span class="badge">${deprem.siddet}</span></td>
       </tr>
     `;
-
-
     tableBody.innerHTML += row;
   });
 }
-
 function fetchDeprems() {
   const today = new Date();
   const yesterday = new Date(today);
   yesterday.setDate(yesterday.getDate() - 1);
-
   const start = yesterday.toISOString().slice(0, 19).replace("T", " ");
   const end = today.toISOString().slice(0, 19).replace("T", " ");
-
   const url = `https://cors-anywhere.herokuapp.com/https://deprem.afad.gov.tr/apiv2/event/filter?start=${start}&end=${end}&limit=10000&offset=5&orderby=timedesc`;
-
-
-
   fetch(url)
   .then((response) => {
     if (!response.ok) {
@@ -84,45 +70,23 @@ function fetchDeprems() {
   });
 
 }
-
 fetchDeprems();
-
-
-
-
-
-
-
-
-
-
-
-
-// Get the table and the buttons
 const table = document.getElementById("myTable");
 const filter1Button = document.getElementById("filter1");
 const filter2Button = document.getElementById("filter2");
 const filter3Button = document.getElementById("filter3");
-
-// Add event listeners to the buttons
 filter1Button.addEventListener("click", function() {
   filterTable(table, 4, null, false);
 });
-
 filter2Button.addEventListener("click", function() {
   filterTable(table, 4, 3, true);
 });
-
 filter3Button.addEventListener("click", function() {
   filterTable(table, null, 3, false);
 });
-
 filter4Button.addEventListener("click", function() {
   filterTable(table,-Infinity, Infinity);
   });
-
-
-// Function to filter the table based on the intensity values
 function filterTable(table, max, min, between) {
   for (let i = 1; i < table.rows.length; i++) {
     let intensity = parseFloat(table.rows[i].cells[3].textContent);
@@ -143,115 +107,63 @@ function filterTable(table, max, min, between) {
     }
   }
 }
-
-
-
-
 function showMap(lat, lng, konum, buyukluk, tarih) {
-
-
 const konumPopup = document.getElementById("konumPopup");
 const tarihPopup = document.getElementById("tarihPopup");
 const siddetPopup = document.getElementById("siddetPopup");
 const tehlikeDurumuPopup = document.getElementById("tehlikeDurumuPopup");
 const Yapilacaklarr = document.getElementById("Yapilacaklarr");
-
-
-
 const morkutucuk = document.getElementById("morkutucuk");
-
 morkutucuk.classList.add("animate__animated");
-
 konumPopup.innerHTML = konum;
 tarihPopup.innerHTML = tarih;
 siddetPopup.innerHTML = buyukluk;
-
-
 if (buyukluk>6) {
  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-danger" >Tehlikeli</span> Olarak nitelendirildi.';
  Yapilacaklarr.innerHTML = '<li>Bölgedeki çoğu ev yıkılmış olabilir!</li><li>Yardıma ihtiyacı olan insanlar olabilir!</li><li>Çevre illerden de hissedilen güçlü bir deprem! Yerel afet birimlerine bilgi verin!</li>';
 }
-
 if (buyukluk>4 && buyukluk<6) {
  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-secondary" >Şiddetli</span> Olarak nitelendirildi.';
  Yapilacaklarr.innerHTML = '<li>Çoğu kişinin hissettiği güçlü bir sarsılma yaşandı.</li><li>Problemli ve eski evler yıkılmış olabilir.</li><li>Bazı eşyalar düşmüş olabilir.</li><li>Yardıma ihtiyacı olan insanlar bulunabilir.</li>';
 }
-
 if (buyukluk<4) {
  tehlikeDurumuPopup.innerHTML = '<span class="badge badge-primary" >Hafif Sarsıntı</span> Olarak nitelendirildi.';
  Yapilacaklarr.innerHTML = '<li>Bazı kişiler hafif bir sarsılma hissetmiş olabilir.</li><li>Ciddi bir sarsıntı olmadı</li>';
 }
-
-
-
 const src = `https://maps.google.com/maps?q=${lat},${lng}&z=9&output=embed`;
 document.querySelector("iframe").src = src;
 const mapGoster = document.getElementById("mapGoster");
 mapGoster.style.display="flex";
-
 }
-
-
-
-
 function showMapClose() {
-
-
-
 const mapGoster = document.getElementById("mapGoster");
 mapGoster.style.display="none";
-
-
-
-
 }
-
-
-
-
 var darkModeMu = 0;
-
 function darkModeGecis() {
-
-
   if (darkModeMu == 0) {
   halfmoon.toggleDarkMode();
-
   document.querySelector('.content-wrapper').style.removeProperty('background-color');
   document.querySelector('#morkutucuk').style.removeProperty('background-color');
   document.querySelector('#morkutucuk').style.removeProperty('border');
   document.querySelector('#kapatbutton').style.removeProperty('backgroundColor');
   document.querySelector('#kapatbutton').style.removeProperty('color');
-
   document.querySelector('.content-wrapper').style.setProperty('background-color', 'white');
-  
   document.querySelector('#morkutucuk').style.setProperty('background-color', 'white');
-  
   document.querySelector('#morkutucuk').style.setProperty('border', '1px solid black');
-  
   document.querySelector('#kapatbutton').style.setProperty('background-color', 'white');
-  
   darkModeMu++;
 }else {
   halfmoon.toggleDarkMode();
-
   document.querySelector('.content-wrapper').style.removeProperty('background-color');
   document.querySelector('#morkutucuk').style.removeProperty('background-color');
   document.querySelector('#morkutucuk').style.removeProperty('border');
   document.querySelector('#kapatbutton').style.removeProperty('backgroundColor');
   document.querySelector('#kapatbutton').style.removeProperty('color');
-
-
 document.querySelector('.content-wrapper').style.setProperty('background-color', 'black');
-
 document.querySelector('#morkutucuk').style.setProperty('background-color', 'black');
-
 document.querySelector('#morkutucuk').style.setProperty('border', '1px solid #6f009d');
-
 document.querySelector('#kapatbutton').style.setProperty('background-color', '#6f009d');
-
 darkModeMu = 0;
 }
-
-
 }
